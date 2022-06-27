3b0e09a2016df91dcc1f7fcf8f3e1d4105744362
Uncle_Iroh <vadegtyarev@gmail.com>
2022-06-27 15:06:38 +0500

    Example011_MethodErray

---
+++ Example011_MethodErray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 29, 38, 47, 56, 92, 65, 74, 843, 92 };
+
+int n = array.Length;
+int find = 92;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++; 
+}
