9ee7f982e9eb5cf494b8337352d4175f9fba79ac
Uncle_Iroh <vadegtyarev@gmail.com>
2022-07-15 11:24:51 +0300

    Example016_RecursionAlgoritm

--- Example016_RecursionAlgoritm/Program.cs
+++ Example016_RecursionAlgoritm/Program.cs
@@ -14,26 +14,26 @@
 
 // int[,] matrix = new int[3,4]; 
 
-// // for (int rows = 0; rows < 3; rows++)
-// // {
-// //     for (int columns = 0; columns < 4; columns++)
-// //     {
-// //         Console.Write($"-{matrix[rows, columns]}-");
-// //     }
-// //     Console.WriteLine();
-// // }
+// for (int rows = 0; rows < 3; rows++)
+// {
+//     for (int columns = 0; columns < 4; columns++)
+//     {
+//         Console.Write($"-{matrix[rows, columns]}-");
+//     }
+//     Console.WriteLine();
+// }
 
-// // void PrintArray(int[,] matr)
-// // {
-// //     for (int rows = 0; rows < matr.GetLength(0 ); rows++)
-// //     {
-// //     for (int columns = 0; columns < matr.GetLength(1); columns++)
-// //         {
-// //         Console.Write($" {matr[rows, columns]} ");
-// //         }
-// //     Console.WriteLine();
-// //     }
-// // }
+// void PrintArray(int[,] matr)
+// {
+//     for (int rows = 0; rows < matr.GetLength(0 ); rows++)
+//     {
+//     for (int columns = 0; columns < matr.GetLength(1); columns++)
+//         {
+//         Console.Write($" {matr[rows, columns]} ");
+//         }
+//     Console.WriteLine();
+//     }
+// }
 
 // void FillArray (int[,] matr)
 // {
